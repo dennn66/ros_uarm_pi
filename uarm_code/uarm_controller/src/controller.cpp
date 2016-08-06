df83725776a68917473a935732e4d8df14f2fcb9
dennn <dennn6696@gmail.com>
2016-08-06 17:00:37 +0500

    servo velocity control

--- uarm_code/uarm_controller/src/controller.cpp
+++ uarm_code/uarm_controller/src/controller.cpp
@@ -16,9 +16,20 @@ Controller::Controller(){
     servo_type[3] =  pca9685_msgs::ServoState::D009A;
     servo_type[4] =  pca9685_msgs::ServoState::D009A;
 
-
+    servo_needs_hold[0] =  1;
+    servo_needs_hold[1] =  1;
+    servo_needs_hold[2] =  0;
+    servo_needs_hold[3] =  0;
+    servo_needs_hold[4] =  0;
         node.param("offsetL", offsetL, 0.33);
         node.param("offsetR", offsetR, -0.20);
+
+	    target_position[0] = current_position[0] = 0+offsetR;
+	    target_position[1] = current_position[1] = 0+offsetL;
+	    target_position[2] = current_position[2] = 0;
+	    target_position[3] = current_position[3] = 0;
+	    target_position[4] = current_position[4] = HAND_ANGLE_OPEN;
+
 
 	sub_joints_position = node.subscribe("uarm/target_position", 100, &Controller::chatterJointsState, this);
 
@@ -31,20 +42,75 @@ void Controller::chatterJointsState (const uarm_msgs::JointsConstPtr &uarm_jnts)
     ROS_INFO("Recieved uarm position");
     pca9685_msgs::ServoState msg;
 
-    position[0] = uarm_jnts->angle_r+offsetR;
-    position[1] = uarm_jnts->angle_l+offsetL;
-    position[2] = uarm_jnts->angle_rot;
-    position[3] = uarm_jnts->angle_hand_rot;
-    position[4] = uarm_jnts->angle_grip;
-    
- 
-    for(int port = 0; port < N_CHANNELS; port++){
-        msg.port_num = channels[port];  
-        msg.servo_rot = position[port];
-        msg.servo_type =  servo_type[port];
-        ROS_INFO("Servo %d [%f]",  channels[port], position[port]);
-        pub_servo_position.publish(msg);
-    }
+    target_position[0] = uarm_jnts->angle_r+offsetR;
+    target_position[1] = uarm_jnts->angle_l+offsetL;
+    target_position[2] = uarm_jnts->angle_rot;
+    target_position[3] = uarm_jnts->angle_hand_rot;
+    target_position[4] = uarm_jnts->angle_grip;
+
+}
+
+double Controller::getDelta(double current_pos, double target_pos){
+	double step;
+	
+	//#define ANGLE_PRECISION        0.001
+        //#define ANGLE_STEP        0.001
+
+	if(current_pos < target_pos){
+		if(target_pos - current_pos > ANGLE_PRECISION){
+			if(target_pos - current_pos > ANGLE_STEP){
+				step = ANGLE_STEP;
+			} else {
+				step = target_pos - current_pos;
+			}
+		} else {
+				step = 0;
+		}
+	} else {
+		if(current_pos - target_pos > ANGLE_PRECISION){
+			if(current_pos - target_pos > ANGLE_STEP){
+				step = -ANGLE_STEP;
+			} else {
+				step = target_pos - current_pos;
+			}
+		} else {
+				step = 0;
+		}
+	}
+
+	return(step);
+}
+
+
+void Controller::controller (){
+
+    pca9685_msgs::ServoState msg;
+
+	while (node.ok()){
+	    for(int port = 0; port < N_CHANNELS; port++){
+		msg.port_num = channels[port];  
+
+                double delta;
+                delta = getDelta(current_position[port], target_position[port]);
+                current_position[port] += delta;
+                if(delta != 0) {
+	            msg.servo_rot = current_position[port];
+		    msg.servo_type =  servo_type[port];
+	        } else {
+                    if(servo_needs_hold[port] == 0) {
+		        msg.servo_rot = current_position[port];
+		        msg.servo_type =  0;  // STOP SERVO
+                    } else {
+	                msg.servo_rot = current_position[port];
+		        msg.servo_type =  servo_type[port];
+                    }
+                }
+		ROS_INFO("Servo %d [%f]",  msg.port_num, msg.servo_rot, msg.servo_type);
+		pub_servo_position.publish(msg);
+	    }
+		ros::spinOnce();
+		ros::Rate(25).sleep();
+	}
 }
 
 
@@ -52,6 +118,7 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "uarm_controller");
     Controller c;
+    c.controller();
     ros::spin();
 }
 
--- uarm_code/uarm_controller/src/controller.hpp
+++ uarm_code/uarm_controller/src/controller.hpp
@@ -7,6 +7,15 @@
 #include <pca9685_msgs/ServoState.h>
 #include <uarm_msgs/Joints.h>
 
+#define ARM_ROTATION_MIN        -1.570796327
+#define ARM_ROTATION_MAX        1.570796327
+#define HAND_ROTATION_MIN       -1.570796327
+#define HAND_ROTATION_MAX       1.570796327
+#define HAND_ANGLE_OPEN         -1.134464014
+#define HAND_ANGLE_CLOSE        -0.349065851
+#define ANGLE_PRECISION        0.001
+#define ANGLE_STEP        0.001
+
 /*****************  Port definitions  *****************/
 #define SERVO_R        1
 #define SERVO_L        2
@@ -21,6 +30,7 @@
 class Controller {
 	public:
 		Controller();
+		void controller();
 
 	private:
 
@@ -30,15 +40,18 @@ class Controller {
 		ros::Publisher pub_servo_position;
 
 		int channels[N_CHANNELS];
-                double position[N_CHANNELS];
+                double current_position[N_CHANNELS];
+                double target_position[N_CHANNELS];
                 int servo_type [N_CHANNELS];
+                int servo_needs_hold [N_CHANNELS];
+                int servo_hold [N_CHANNELS];
 
                 void chatterJointsState (const uarm_msgs::JointsConstPtr &uarm_jnts);
+		double getDelta(double current_pos, double target_pos);
 
 /*******************  Servo offset  *******************/
         double offsetL;
         double offsetR;
- 	/*****************  Define variables  *****************/
 };
 
 
